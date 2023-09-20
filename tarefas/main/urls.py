4e8837718d774eb2f7954a472210eaa7f4099766
Bernardo Perrot <bernardo.sp@aluno.ifsc.edu.br>
2023-09-20 00:24:28 -0300

    aula 19/9

--- tarefas/main/urls.py
+++ tarefas/main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from main import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
+    path('<int:id>/', views.index, name='index'),
+    path('create/', views.create, name='create'),
 ]
--- tarefas/main/views.py
+++ tarefas/main/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import ToDoList, Item
+from .forms import CreateNewList
 
 # Create your views here.
+def create(request):
+    form = CreateNewList()
+    if request.method == 'POST':
+        form = CreateNewList(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            todo = ToDoList(name=name)
+            todo.save()
+            request.user.todolist.add(todo)
+            return redirect("/%i" % todo.id)
+        else:
+            form = CreateNewList()
+
+    return render(request, 'main/create.html', {"form":form})
+    # form = CreateNewList()
+    # return render(request, "main/create.html",{"form":form})
+
+
+
+def index(request, id):
+    lista = ToDoList.objects.get(id=id)
+    return render(request, 'main/list.html', {'lista':lista})
 
 def home(reponse):
     return render(reponse, 'main/home.html')
+
+
